659dd90ef73feaa8daf832bd9cb9ee0d12abc7d9
healthlinegate <97893353+healthlinegate@users.noreply.github.com>
2022-04-20 15:35:13 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <a href="https://www.healthlinegate.com/"><Header title="Health and Wellness" /></a>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
